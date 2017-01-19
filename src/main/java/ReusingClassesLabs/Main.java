654a1cf797b5afccf6fba0adcdc7bc2dacb400c4
Randall Crame <randallcrame@zipcoders-MacBook-Pro.local>
2017-01-18 21:14:27 -0500

    Completed Humans and Supermans

--- src/main/java/ReusingClassesLabs/Main.java
+++ src/main/java/ReusingClassesLabs/Main.java
@@ -9,24 +9,29 @@ public class Main {
         RotateArray object = new RotateArray();
 
         int array[] = {1,2,3,4,5};
-        String original = "Original: ", rotated ="Rotated: ";
-        for(int i:array) original += array[i-1] + " ";
 
+        System.out.println("Original: ");
+        for(int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+        }
+        System.out.println();
 
         object.rotateArray(array, 2);
-        for(Integer j:array) rotated+=array[j-1]+" ";
-
-        for(int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
+        System.out.println("Rotated: ");
+        for(int j = 0; j < array.length; j++) {
+            System.out.print(array[j]);
         }
 
+    Human bob = new Human();
+    SuperHuman steve = new SuperHuman();
+    Human stacy =  new SuperHuman();
 
-        System.out.println(original+"\n"+rotated);
-
-
-        for(int i: array) {
-            System.out.println(i)
-            i = array[..];
-        }
+    bob.setName("jim");
+    System.out.println(bob.getName());
+    steve.setAlignment("Good");
+    System.out.println(steve.getAlignment());
+    stacy.setGender("N/A"); //unable to set SuperHuman fields.
+    System.out.println(stacy.getGender());
     }
 }
+
